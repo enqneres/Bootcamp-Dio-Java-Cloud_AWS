4e91c610bc627f0706614b0573512decd78a65b4
Enoque Neres <enoque.sneres@live.com>
2023-08-12 00:23:59 -0300

    Resolvendo desafio ListaTarefa

---
+++ src/Tarefa.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
